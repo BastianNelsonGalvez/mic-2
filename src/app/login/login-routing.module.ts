f7a9e3b479e257a4e2401698f85a0971af01d09c
Bastian Nelson <bastian.nelson@sermaluc.cl>
2023-11-21 09:14:08 -0300

    feat:add login and profile

---
+++ src/app/login/login-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginPage } from './login.page';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginPage,
+  },
+  {
+    
+      path: 'tabs',
+      loadChildren: () => import('../tabs/tabs.module').then(m => m.TabsPageModule)
+    
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class LoginRoutingModule {}
---
+++ src/app/login/login.module.ts
@@ -0,0 +1,15 @@
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { LoginRoutingModule } from './login-routing.module';
+
+import { LoginPage } from './login.page';
+import { TabsPageModule } from '../tabs/tabs.module';
+
+@NgModule({
+  imports: [IonicModule, CommonModule, FormsModule, LoginRoutingModule,TabsPageModule],
+  declarations: [LoginPage],
+})
+export class LoginPageModule {}
